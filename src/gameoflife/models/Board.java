cefdae774f9157e892178bf312ca4abb5f901830
Ara Ekmekjian <ekmekjian.ara@protonmail.com>
2020-11-30 22:48:41 -0500

    initialized Board default constructor

--- src/gameoflife/models/Board.java
+++ src/gameoflife/models/Board.java
@@ -5,8 +5,10 @@ public class Board {
     private static int _ySize = 10;
     private Cell [][] cells = new Cell[_xSize][_ySize];
     public Board(){
-        for(int i =0;i<cells.length;i++) {
-
+        for(int i=0;i< _xSize;i++) {
+            for(int j=0;j<_ySize;j++){
+               cells[i][j] = new Cell();
+            }
         }
     }
 }
